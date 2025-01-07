e1183dbd7b1def527fba5ddfb0d86d3e342fc1c1
khusbookumari <86683450+khusbookumari@users.noreply.github.com>
2025-01-07 12:48:24 +0530

    Add files via upload

---
+++ KhusboosFolder/insert_script_chinese_char.sql
@@ -0,0 +1,5 @@
+INSERT INTO DummyTable (ID, Name, Age, Email, CreatedDate)
+VALUES (2, '李娜', 25, 'lina@example.com', SYSDATE);
+INSERT INTO DummyTable (ID, Name, Age, Email, CreatedDate)
+VALUES (3, '王强', 28, 'wangqiang@example.com', SYSDATE);
+COMMIT;
